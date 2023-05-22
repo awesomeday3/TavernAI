8310023b3f2cc4f90de4482669efe1cd4dd24745
Aww <127321461+awesomeday3@users.noreply.github.com>
2023-05-22 17:42:49 +0900

    Chat import fix

--- server.js
+++ server.js
@@ -1797,7 +1797,7 @@ app.post("/importchat", urlencodedParser, function(request, response){
 
         var format = request.body.file_type;
         let filedata = request.file;
-        let avatar_url = (request.body.avatar_url).replace(`.${characterFormat}`, '');
+        let avatar_url = (request.body.filename).replace(`.${characterFormat}`, '');
         let ch_name = request.body.character_name;
         //console.log(filedata.filename);
         //var format = request.body.file_type;
@@ -1965,6 +1965,7 @@ app.post("/importchat", urlencodedParser, function(request, response){
                     let jsonData = json5.parse(line);
                     
                     if(jsonData.user_name !== undefined){
+			    
                         fs.copyFile('./uploads/'+filedata.filename, chatsPath+avatar_url+'/'+Date.now()+'.jsonl', (err) => {
                             if(err) {
                                 response.send({error:true});
